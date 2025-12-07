6a7325ce41589c707d85fb0dec1e15f04783f89d
Ahmad Rofi' Izzulhaq <ahrofzulcaruban@gmail.com>
2025-12-07 16:01:04 +0700

    feat: logic umur

--- tugas.js
+++ tugas.js
@@ -1,1 +1,11 @@
 console.log("Program Validasi Klub Malam");
+
+const prompt = require("prompt-sync")();
+
+const umur = Number(prompt("Masukkan umur: "));
+
+if (umur < 21) {
+    console.log("Tidak boleh masuk ");
+}
+
+console.log("Umur valid. Lanjut ke pengecekan uang...");
